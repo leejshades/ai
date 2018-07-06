1dce92c80bf77270781f6f95a3b3b41344b4ce55
hadeslee <7189444+hadesleejs@users.noreply.github.com>
2018-07-06 09:29:09 +0800

    init index
    Co-Authored-By: hadeslee <hadesleejs@users.noreply.github.com>

--- apps/sight/views.py
+++ apps/sight/views.py
@@ -5,11 +5,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.base import View
 
+
+class IndexView(View):
+    def get(self,request):
+        return HttpResponse("it's ok!")
+
+
 class UploadView(View):
+
     def get(self,request):
         return HttpResponse('this is get method')
+
     def post(self,request):
         return HttpResponse('this is post method')
+
 
 def upload(request):
     if request.method == 'POST':
